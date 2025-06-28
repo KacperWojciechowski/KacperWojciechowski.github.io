725a6c9f49a5fa13b1e4d8f1a117668e08b854e8
Kacper Wojciechowski <k.woj.coding@gmail.com>
2025-06-28 17:44:40 +0200

    added timeline

---
+++ index.js
@@ -0,0 +1,20 @@
+function adjustIframeHeight() {
+    var iframe = document.querySelector('.main-window');
+    var footerHeight = document.querySelector('.footer').offsetHeight;
+    var bannerHeight = document.querySelector('.banner').offsetHeight;
+    var iframeDocument = iframe.contentWindow.document;
+
+    // Reset the iframe height to auto to get the correct scrollHeight
+    iframe.style.height = 'auto';
+
+    var contentHeight = iframeDocument.body.scrollHeight;
+    var availableHeight = window.innerHeight - bannerHeight - footerHeight;
+
+    if (contentHeight <= availableHeight) {
+        iframe.style.height = availableHeight + 'px';
+    } else {
+        iframe.style.height = contentHeight + footerHeight + 20 + 'px';
+    }
+
+    iframe.style.overflow = 'hidden'; // Make iframe not scrollable
+}
