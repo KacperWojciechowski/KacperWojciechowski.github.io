6b47916ae62c1051ed16b7b87edcb0891a180ecc
Kacper Wojciechowski <k.woj.coding@gmail.com>
2025-06-29 23:46:31 +0200

    refactored and automated skill table building

---
+++ scripts/skills-data.js
@@ -0,0 +1,213 @@
+const programmingLanguages = {
+    0: {
+        0: {
+            "C++": [
+                "Standards: 11, 14, 17, 20",
+                "STL containers and algorithms",
+                "Concurrency - synchronization, multithreading",
+                "Memory algignment",
+                "Memory management - RAII, memory ownership, smart pointers side effects",
+                "Templates - metaprogramming, template specialization, Curriously Recurring Template Pattern, compile-time conditionals",
+                "Lambda functions and function pointers",
+                "Functors",
+                "G++ compiler attributes",
+                "GTest - regular, parametrized and typed tests",
+                "Structured bindings",
+                "C++ Guidelines and good practices"
+            ],
+            "TTCN3": [
+                "Designing and implementing flow-container-based test frameworks",
+                "Operating on components",
+                "Creating and expanding templates",
+                "Organizing modules and managing dependencies",
+                "Message-based communication between components",
+                "Parallel message reception"
+            ],
+            "Rust": [
+                "Currently learning",
+            ],
+            "Python": [
+                "Working with fundamental data structures - lists, tuples, sets and dictionaries",
+                "Creating and using classes and objects",
+                "Using and managing external libraries",
+            ],
+            "Bash": [
+                "Creating automatization scripts and small tools",
+                "Creating parametrized scripts",
+                "Creating scripts operating on files and directories",
+                "Creating script-invoking commands using aliases",
+                "Using environment variables"
+            ]
+        }
+    }
+};
+
+const embedded = {
+    0: {
+        "Real-time operating systems": {
+            "TI-RTOS": [
+                "Understanding of task scheduling and synchronization",
+                "Task priority, preemption and context switching",
+                "Managing task stack size",
+                "Handlers for interrupt routines and events such as task termination"
+            ],
+            "FreeRTOS": [
+                "Understanding of task scheduling and synchronization",
+                "Task priority, preemption and context switching",
+                "Managing task stack size",
+                "Handlers for interrupt routines and events such as task termination"
+            ]
+        },
+        "Microcontrollers": {
+            "STM32": [
+                "Utilizing HAL library for peripheral management",
+                "Configuring peripherals using MCU registers",
+                "Creating bare metal applications",
+                "Implementing communication using USART",
+                "Implementing ISR handling routines",
+                "Implementing PWM control for DC motors",
+                "Configuring DMA for data transfer with peripherals",
+                "Configuring LwIP routines",
+                "Utilizing GPIO for input and output",
+            ],
+            "Arduino": [
+                "Utilizing GPIO for input and output",
+                "Implementing PWM control for DC motors",
+                "Implementing ISR handling routines",
+                "Implementing timer-based concurrency framework",
+                "Implementing communication using UART and I2C",
+            ]
+        },
+        "Hardware" : {
+            "Components": [
+                "Working with DC and BLDC motors",
+                "Working with logic gates",
+                "Working with logic level converters",
+                "Working with external sensors utilizing I2C, UART and GPIO",
+                "Working with relays"
+            ],
+            "Tools": [
+                "Soldering components",
+                "Analyzing signals using oscilloscope",
+                "Measuring power and component parameters using multimeter",
+            ]
+        }
+    }
+};
+
+const tools = {
+    0: {
+        "Operating systems": {
+            "Linux": [
+                "Managing files and directories using terminal",
+                "Creating symbolic links for files and directories",
+                "Practical experience with building projects in a Linux-based environment",
+                "Using Linux native tools for developing custom Bash scripts",
+                "Managing Linux packages using apt",
+                "Hands-on experience with Ubuntu",
+            ],
+            "Windows": [
+                "Hands-on experience with Windows systems starting from Windows XP up to Windows 11",
+                "Hands-on experience with WSL2 with Ubuntu instance"    
+            ]
+        },
+        "Code analysis and quality": {
+            "Clang-format" : [
+                "Configuring Clang-format for tailored code formatting",
+                "Integrating Clang-format with Git hooks for automated formatting"
+            ],
+            "Gdb": [
+                "Debugging C++ application crashes using Gdb and backtraces",
+            ]
+        },
+        "Documentation": {
+            "Generated": [
+                "Generating documentation using Doxygen",
+            ],
+            "Hand-made": [
+                "Creating descriptive documentation using Markdown",
+                "Basic knowledge of AsciiDoc",
+                "Hands-on experience with creating and maintaining PlantUML diagrams",
+                "Hands-on experience with designing and analyzing UML flow diagrams"
+            ]
+        },
+    },
+    1: {
+        "Version control systems": {
+            "Git": [
+                "Creating and managing repositories",
+                "Branching and merging strategies",
+                "Using Git hooks for automation",
+                "Collaborating using pull requests",
+                "Managing conflicts and rebasing",
+                "Operating git-based web platforms such as GitHub, GitLab, BitBucket and Gerrit"
+            ],
+            "Known Git-based repositories": [
+                "GitHub - Managing repositories, issues and pull requests",
+                "GitLab - Managing repositories, issues, issue boards, labels and pull requests",
+                "Gerrit - Managing changes, performing code review"
+            ]
+        },
+        "Build and dependency management systems": {
+            "CMake": [
+                "Creating, building and maintaining projects based on CMake scripts",
+            ],
+            "Cargo": [
+                "Currently learning"
+            ]
+        },
+        
+        "AI assistants": {
+            "GitHub Copilot": [
+                "Hands-on experience with code design and refactoring using GitHub Copilot",
+                "Hands-on experience with accelerated troubleshooting with help of GitHub Copilot"
+            ],
+            "ChatGPT": [
+                "Basic experience with information search using ChatGPT"
+            ]
+        },
+        
+    }
+};
+    
+
+const knowhow = {
+    0: { 
+        0: {
+            "Software design": [
+                "Utilizing SOLID principles",
+                "Designing and implementing finite state machines",
+                "Utilizing design patterns",
+                "Familiarity with Clean Code principles",
+                "Hands-on experience with identifying and fixing side effects",
+                "Hands-on experience with refactoring legacy code",
+                "Hands-on experience with Test Driven Development"
+            ],
+            "Digital circuits:": [
+                "Designing circuits using logic gates and latches",
+                "Understanding of basics of software-hardware functional decomposition",
+            ],
+            "Machine learning": [
+                "Understanding of shallow learning algorithms",
+                "Understanding of neural network models, such as FFNN, CNN and RNN",
+                "Experience with analyzing model quality metrics",
+                "Hands-on experience with preprocessing data for training",
+                "Slight experience with training models using TensorFlow",
+                "Slight experience with training models using Shark-ML, Shogun and DLib"
+            ],
+            "Graph theory": [
+                "Understanding of graph theory basics",
+                "Hands-on experience with implementing graph library in C++",
+                "Experience with solving problems such as finding shortest path, coloring, topological sort and traversing graphs with C++"
+            ],
+            "Telecommunication": [
+                "Understanding of Carrier Aggregation functionality",
+                "Understanding of Dual Connectivity functionality",
+                "Understanding of 5G RAN architecture, including CU-DU split and CP-UP split",
+                "Understanding of SCTP protocol",
+                "Hands-on experience with troubleshooting and analyzing 5G RAN issues",
+                "Hands-on experience with utilizing 3GPP documentation"
+            ]
+        }
+    }
+}
--- scripts/skills.js
+++ scripts/skills.js
@@ -1,102 +1,80 @@
-const programmingLanguagesCompiled = {
-    "C++": [
-        "Standards: 11, 14, 17, 20",
-        "STL containers and algorithms",
-        "Concurrency - synchronization, multithreading",
-        "Memory algignment",
-        "Memory management - RAII, memory ownership, smart pointers side effects",
-        "Templates - metaprogramming, template specialization, Curriously Recurring Template Pattern, compile-time conditionals",
-        "Lambda functions and function pointers",
-        "Functors",
-        "G++ compiler attributes",
-        "GTest - regular, parametrized and typed tests",
-        "Structured bindings",
-        "C++ Guidelines and good practices"
-    ],
-    "TTCN3": [
-        "Designing and implementing flow-container-based test frameworks",
-        "Operating on components",
-        "Creating and expanding templates",
-        "Organizing modules and managing dependencies",
-        "Message-based communication between components",
-        "Parallel message reception"
-    ],
-    "Rust": [
-        "Currently learning",
-    ],
-    "Python": [
-        "Working with fundamental data structures - lists, tuples, sets and dictionaries",
-        "Creating and using classes and objects",
-        "Using and managing external libraries",
-    ],
-    "Bash": [
-        "Creating automatization scripts and small tools",
-        "Creating parametrized scripts",
-        "Creating scripts operating on files and directories",
-        "Creating script-invoking commands using aliases",
-        "Using environment variables"
-    ]
-};
+function renderHorizontalTable(table) {
+    let theadLayers = [];
+    let tbodyLayers = [];
 
-const tools = {
-    "Build systems": [
-        "CMake - creating, building and maintaining projects based on CMake scripts",
-    ],
-    "Operating systems": {
-        "Linux": [
-            "Managing files and directories using terminal",
-            "Creating symbolic links for files and directories",
-            "Practical experience with building projects in a Linux-based environment",
-            "Using Linux native tools for developing custom Bash scripts",
-            "Managing Linux packages using apt",
-            "Hands-on experience with Ubuntu",
-        ],
-        "Windows": [
-            "Hands-on experience with Windows systems starting from Windows XP up to Windows 11",
-            "Hands-on experience with WSL2 with Ubuntu instance"    
-        ],
-        "TI-RTOS": [
-            "Understanding of task scheduling and synchronization",
-            "Task priority, preemption and context switching",
-            "Managing task stack size",
-            "Handlers for interrupt routines and events such as task termination"
-        ],
-        "FreeRTOS": [
-            "Understanding of task scheduling and synchronization",
-            "Task priority, preemption and context switching",
-            "Managing task stack size",
-            "Handlers for interrupt routines and events such as task termination"
-        ]
-    },
-    "Version control systems": {
-        "Git": [
-            "Creating and managing repositories",
-            "Branching and merging strategies",
-            "Using Git hooks for automation",
-            "Collaborating using pull requests",
-            "Managing conflicts and rebasing",
-            "Operating git-based web platforms such as GitHub, GitLab, BitBucket and Gerrit"
-        ]
-    },
-    "Documentation": [
-        "Generating documentation using Doxygen",
-        "Creating descriptive documentation using Markdown",
-        "Basic knowledge of AsciiDoc",
-        "Hands-on experience with creating and maintaining PlantUML diagrams",
-        "Hands-on experience with designing and analyzing UML flow diagrams"
-    ],
-    "AI assistants": [
-        "Hands-on experience with code design and refactoring using GitHub Copilot",
-        "Basic experience with crafting tailored prompts to acquire desired results",
-        "Basic experience with information search using ChatGPT"
-    ],
-    "Code analysis and quality": {
-        "Clang-format" : [
-            "Configuring Clang-format for tailored code formatting",
-            "Integrating Clang-format with Git hooks for automated formatting"
-        ],
-        "Gdb": [
-            "Debugging C++ application crashes using Gdb and backtraces",
-        ]
+    Object.entries(table).forEach(layer => {
+        let thead = '<thead>';
+        let tbody = '<tbody>';
+        const [, superlabels] = layer;
+        
+        const [superlabel, ] = Object.entries(superlabels).at(0);
+        if (superlabel != 0)
+        {
+            thead += '<tr>';
+            Object.entries(superlabels).forEach(([label, sublabels]) => {
+                thead += `<th colspan="${Object.entries(sublabels).length}">${label}</th>`;
+            });
+            thead += '</tr>'
+        }
+
+        thead += '<tr>';
+        tbody += '<tr>';
+
+        Object.entries(superlabels).forEach(([, sublabels]) => {
+            Object.entries(sublabels).forEach(([label, values]) => {
+                thead += `<th>${label}</th>`;
+                tbody += `<td><ul>${values.map(value => `<li>${value}</li>`).join('')}</ul></td>`;
+            });
+        });
+
+        thead += '</tr></thead>';
+        tbody += '</tr></tbody>';
+        theadLayers.push(thead);
+        tbodyLayers.push(tbody);
+    });
+        
+    let out = "";
+    for (let index = 0; index < theadLayers.length; index++)
+    {
+        out += theadLayers.at(index);
+        out += tbodyLayers.at(index);
     }
+    return `<table>${out}</table>`;
 }
+
+function renderVerticalTable(table)
+{
+    let rows = '';
+        
+    Object.entries(table).forEach(layer => {
+        const [, superlabels] = layer;
+        Object.entries(superlabels).forEach(([, sublabels]) => {
+            Object.entries(sublabels).forEach(([label, values]) => {
+                rows += `<tr><td>${label}</td>`;
+                rows += `<td><ul>${values.map(value => `<li>${value}</li>`).join('')}</ul></td></tr>`;
+            });
+        });
+    });
+    return `
+        <table>
+            <thead>
+                <th>Subject</th><th>Skills</th>
+            </thead>
+            <tbody>
+                ${rows}
+            </tbody>
+        </table>`;
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    this.document.getElementById("prog-lang-table-desktop").innerHTML = '<h2>Programming languages:</h2>' + renderHorizontalTable(programmingLanguages);
+    this.document.getElementById("embedded-table-desktop").innerHTML = '<h2>Embedded systems:</h2>' + renderHorizontalTable(embedded);
+    this.document.getElementById("tools-table-desktop").innerHTML = '<h2>Tools:</h2>' + renderHorizontalTable(tools);
+    this.document.getElementById("knowhow-table-desktop").innerHTML = '<h2>Know-how:</h2>' + renderHorizontalTable(knowhow);
+
+
+    this.document.getElementById("prog-lang-table-mobile").innerHTML = '<h2>Programming languages:</h2>' + renderVerticalTable(programmingLanguages);
+    this.document.getElementById("embedded-table-mobile").innerHTML = '<h2>Embedded systems:</h2>' + renderVerticalTable(embedded);
+    this.document.getElementById("tools-table-mobile").innerHTML = '<h2>Tools:</h2>' + renderVerticalTable(tools);
+    this.document.getElementById("knowhow-table-mobile").innerHTML = '<h2>Know-how:</h2>' + renderVerticalTable(knowhow);
+})
