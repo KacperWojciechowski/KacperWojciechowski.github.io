0e0f02b1c9f753f866c4e85403e2fc96a3b6b522
Kacper Wojciechowski <k.woj.coding@gmail.com>
2025-07-02 20:35:44 +0200

    redesigning about me

--- scripts/about-me.js
+++ scripts/about-me.js
@@ -1,18 +1,19 @@
 const aboutMeCode = [
-    { "keyword" : "template", "brace" : "&lt;", "" : "typename", "type" : "Company", "" : "" },
-    //{ "keyword" : "template", "brace" : "&lt;", "keyword" : "typename", "type" : "Company", "brace" : "&gt;" },
+    { "keyword" : "template", "brace" : "<", "keyword _1" : "typename", "type" : "Company", "brace _1" : ">" },
     { "keyword" : "class", "type" : "KacperWojciechowski", "brace" : "{" },
     { "keyword" : "public:" },
-    { "type code-tab" : "void", "function" : "introduce", "brace" : "()", "keyword" : ";" },
-    { "type code-tab" : "void", "function" : "hire", "brace" : "()", "keyword" : ";" },
+    { "type code-tab" : "void", "function" : "introduce", "brace" : "()", "keyword _1" : ";" },
+    { "type code-tab" : "void", "function" : "hire", "brace" : "()", "keyword _1" : ";" },
     { "keyword" : "private:" },
-    { "keyword code-tab" : "auto", "variable" : "occupation", "keyword" : "=", "string" : "\"Software Engineer\"" },
-    { "keyword code-tab" : "auto", "variable" : "specialization", "keyword" : "=", "string" : "\"Cpp, SW Architecture\"" },
-    { "keyword code-tab" : "auto", "variable" : "degree", "keyword" : "=", "string" : "\"Master's in Computer Science\"" },
+    { "keyword code-tab" : "uint8_t", "variable" : "age", "keyword _1" : "=", "" : "<span id=\"id-age\" class=\"number\"></div>", "keyword _2" : ";" },
+    { "keyword code-tab" : "auto", "variable" : "occupation", "keyword" : "=", "string" : "\"Software Engineer\"", "keyword _1" : ";" },
+    { "keyword code-tab" : "auto", "variable" : "specialization", "keyword" : "=", "string" : "\"Cpp, SW Architecture\"", "keyword _1" : ";" },
+    { "keyword code-tab" : "auto", "variable" : "degree", "keyword" : "=", "string" : "\"Master's in Computer Science\"", "keyword _1" : ";" },
+    { "comment code-tab" : "// Rounding all months to ~30 days"},
     { "keyword code-tab" : "auto", "variable" : "experience", "keyword" : "="},
-    { "function code-double-tab" : "std::chrono::duration_cast", "brace" : "&lt;" },
-    { "type code-tripple-tab" : "std::chrono::years", "brace" : "&gt;(", "" : "<span id=\"id-experience-length\"></span>", "brace" : ")", "keyword" : ".", "function" : "count", "brace" : "()", "keyword" : ";" },
-    { "type" : "std::weak_ptr", "brace" : "&lt;", "type" : "Company", "brace" : "&gt;", "variable" : "employer", "keyword" : ";" },
+    { "function code-double-tab" : "std::chrono::duration_cast", "brace" : "(" },
+    { "type code-tripple-tab" : "std::chrono::years", "brace _1" : ">", "brace _2" : "(", "" : "<span id=\"id-experience\" class=\"number\"></span>", "brace _3" : ")", "keyword _1" : ".", "function" : "count", "brace" : "()", "keyword" : ";" },
+    { "type code-tab" : "std::weak_ptr", "brace" : "<", "type" : "Company", "brace _1" : ">", "variable" : " employer", "keyword" : ";" },
     { "brace" : "}", "keyword" : ";" },
 ]
 
@@ -21,13 +22,62 @@ const terminalSeparator = [
 ]
 
 const terminalContent = [
-    { "terminal-prompt" : "Ubuntu(~/repos/cv/build)$", "command" : "g++ -o out main.cpp kacperwojciechowski.cpp && ./out" },
+    { "terminal-prompt" : "Ubuntu(~/repos/cv/build)$", "command" : "g++ -o out main.cpp kacperwojciechowski.cpp > /dev/null && ./out" },
     { "terminal-output" : "Hello, my name is Kacper. Interrested in hiring me, aren't you ?" },
     { "terminal-prompt" : "Ubuntu(~/repos/cv/build)$", "cursor" : "<span id=\"terminal-cursor\">|</span>" }
 ]
 
+function calculateDateDiffTillNow(startDate, endDate = new Date()) {
+    let age = endDate.getFullYear() - startDate.getFullYear();
+    const monthDiff = endDate.getMonth() - startDate.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && endDate.getDate() < startDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+function calculateDateDiffWithMonths(startDate, endDate = new Date()) {
+    let age = (endDate.getFullYear() - startDate.getFullYear()) * 12;
+    age += endDate.getMonth() - startDate.getMonth();
+    if (endDate.getDate() < startDate.getDate()) {
+        age--;
+    }
+    return [age/12, age % 12];
+}
+
+const experience = [
+    [new Date(2021, 6, 1), new Date(2021, 7, 1)],
+    [new Date(2021, 9, 1), new Date(2022, 1, 28)],
+    [new Date(2022, 11, 1), new Date()]
+]
+
+function calculateExperience() {
+    let totalExperience = [0, 0];
+    for (let i = 0; i < experience.length; i++) {
+        const [start, end] = experience[i];
+        const periodExperience = calculateDateDiffWithMonths(start, end);
+        totalExperience[0] += periodExperience[0];
+        totalExperience[1] += periodExperience[1];
+        if (totalExperience[1] >= 12) {
+            totalExperience[0] += Math.floor(totalExperience[1] / 12);
+            totalExperience[1] = totalExperience[1] % 12;
+        }
+    }
+    return totalExperience[0] + "y + " + totalExperience[1] + "*30d";
+}
+
 window.addEventListener('DOMContentLoaded', function() {
-    this.document.getElementById("about-me-code").innerHTML = renderCode(aboutMeCode);
+    this.document.getElementById("about-me-code").innerHTML = renderCode(aboutMeCode, noSpaceCppKeywords);
     this.document.getElementById("terminal-separator").innerHTML = renderTerminal(terminalSeparator);
     this.document.getElementById("about-me-terminal").innerHTML = renderTerminal(terminalContent);
+    this.document.getElementById("id-age").innerHTML = Math.trunc(calculateDateDiffWithMonths(new Date(1999, 7, 27))[0]);
+    this.document.getElementById("id-experience").innerHTML = calculateExperience();
+
+    this.setInterval(function() {
+        const cursor = this.document.getElementById("terminal-cursor");
+        if (cursor) {
+            cursor.style.visibility = (cursor.style.visibility === 'hidden' ? '' : 'hidden');
+        }
+    }, 500);
 });
+
--- scripts/code.js
+++ scripts/code.js
@@ -1,12 +1,30 @@
-function renderCode(code) {
+const noSpaceCppKeywords = [
+    ";", ".", "<", ">", "(", ")", "()"
+]
+
+function buildOffset(char, length) {
+    let out = '';
+    for (let i = 0; i < length; i++) {
+        out += char;
+    }
+    return out;
+}
+
+function renderCode(code, noSpaceKeywords) {
     out = '';
     const lineCount = code.length;
+
+    let digits = lineCount.toString().length;
     for (let i = 0; i < lineCount; i++) {
+        out += `<span class="comment">${(i+1).toString().padStart(digits, buildOffset('0', digits - i.toString().length))} |</span> `;
+        
         const line = code[i];
         for (let j = 0; j < Object.entries(line).length; j++) {
             const [key, value] = Object.entries(line)[j];
             out += `<span class="${key}">${value}</span>`;
-            if (j < Object.entries(line).length - 1) {
+            if ((noSpaceKeywords && noSpaceKeywords.includes(value)) || (noSpaceKeywords && j+1 < Object.entries(line).length &&  noSpaceKeywords.includes(Object.entries(line)[j+1][1]))) {
+            }
+            else if (j < Object.entries(line).length - 1) {
                 out += ' ';
             }
         }
